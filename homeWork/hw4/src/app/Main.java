c41ae98aefbe66c94cc2d814b8b20d4363e45716
Bogdan282 <bogdan673876@gmail.com>
2025-02-03 21:59:15 +0200

    HomeWork4 v4

--- homeWork/hw4/src/app/Main.java
+++ homeWork/hw4/src/app/Main.java
@@ -3,22 +3,20 @@ package app;
 public class Main {
     public static void main(String[] args) {
         int days;
-        float totalEUR, byDay;
-
-        //Вираховування прибутку в день маючи кількість днів та загальний прибуток
+        double totalEUR, byDay;
 
         days = 5;
-        byDay = 2430.68f;
+        byDay = 2430.68;
         totalEUR = days * byDay;
-        System.out.println("Product No 1: smartphone," +
-                "\ntotal sales for " + days + " days is EUR " + totalEUR + "," +
-                "\nsales by day is EUR " + byDay + ".");
+        System.out.printf("Product No 1: smartphone,%n" +
+                "total sales for %d days is EUR %,.2f,%n" +
+                "sales by day is EUR %,.2f.%n", days, totalEUR, byDay);
 
         days = 7;
-        byDay = 1498.12f;
+        byDay = 1498.12;
         totalEUR = days * byDay;
-        System.out.println("Product No 2: laptop," +
-                "\ntotal sales for " + days + " days is EUR " + totalEUR + "," +
-                "\nsales by day is EUR " + byDay + ".");
+        System.out.printf("Product No 2: laptop,%n" +
+                "total sales for %d days is EUR %,.2f,%n" +
+                "sales by day is EUR %,.2f.%n", days, totalEUR, byDay);
     }
 }
