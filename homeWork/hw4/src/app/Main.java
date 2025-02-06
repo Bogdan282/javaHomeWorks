512d076c625bd9251b4634a017e043d76441451f
Bogdan282 <bogdan673876@gmail.com>
2025-02-06 16:33:49 +0200

    HomeWork4 Final

--- homeWork/hw4/src/app/Main.java
+++ homeWork/hw4/src/app/Main.java
@@ -3,22 +3,29 @@ package app;
 public class Main {
     public static void main(String[] args) {
         int days;
-        float totalEUR, byDay;
+        double totalEUR, byDay;
 
-        //Вираховування загального прибутку маючи кількість днів та прибуток за 1 день
+        //Сума продажів товару дорівнює добутку кількості (шт) та ціни.
+
+        //Product No 1: smartphone,
+        //total sales for 5 days is EUR 12153,41,
+        //sales by day is EUR 2430,68.
+        //Product No 2: laptop,
+        //total sales for 7 days is EUR 10486,85,
+        //sales by day is EUR 1498,12.
 
         days = 5;
-        byDay = 2430.68f;
+        byDay = 2430.68;
         totalEUR = days * byDay;
-        System.out.println("Product No 1: smartphone," +
-                "\ntotal sales for " + days + " days is EUR " + totalEUR + "," +
-                "\nsales by day is EUR " + byDay + ".");
+        System.out.println("Product No 1: smartphone,");
+        System.out.printf("total sales for %d days is EUR %.2f,%n", days, totalEUR);
+        System.out.printf("sales by day is EUR %.2f.%n", byDay);
 
         days = 7;
-        byDay = 1498.12f;
+        byDay = 1498.12;
         totalEUR = days * byDay;
-        System.out.println("Product No 2: laptop," +
-                "\ntotal sales for " + days + " days is EUR " + totalEUR + "," +
-                "\nsales by day is EUR " + byDay + ".");
+        System.out.println("Product No 2: laptop,");
+        System.out.printf("total sales for %d days is EUR %.2f,%n", days, totalEUR);
+        System.out.printf("sales by day is EUR %.2f.%n", byDay);
     }
 }
