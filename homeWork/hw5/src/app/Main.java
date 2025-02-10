4bba99eae026ee1ac8e66852d6397a176d70038e
Bogdan282 <bogdan673876@gmail.com>
2025-02-10 04:50:55 +0200

    HomeWork5 V3

--- homeWork/hw5/src/app/Main.java
+++ homeWork/hw5/src/app/Main.java
@@ -7,16 +7,17 @@ public class Main {
     public static void main(String[] args) {
 //        int money;
 
-        System.out.println("Доходи оподатковуются за прогресивною шкалою оподаткування: \n" +
-                "від 0 до 10000, включно, за ставкою 2,5%.\n" +
-                "від 10000, виключно, до 25000, включно, за ставкою 4,3%.\n" +
-                "більше 25000, за ставкою 6,7%. \n");
+        System.out.println("""
+                Доходи оподатковуются за прогресивною шкалою оподаткування:\s
+                від 0 до 10000, включно, за ставкою 2,5%.
+                від 10000, виключно, до 25000, включно, за ставкою 4,3%.
+                більше 25000, за ставкою 6,7%.\s
+                """);
 
 
         taxRate(3200);
         taxRate(12590);
         taxRate(32000);
-
 
 /*        money = 7200;
         System.out.println("Наприклад якщо ваш дохід буде 7200 гривень, то податок становитиме: " + (money <= 10000 ? "2,5%" : "") + (money > 10000 && money < 25000 ? "4,3%" : "") + (money > 25000 ? "6,7%" : ""));
@@ -33,8 +34,6 @@ public class Main {
     }
     public static void taxRate(int money){
         double taxRate = (money <= 10000) ? 2.5 : (money <= 25000) ? 4.3 : 6.7;
-        System.out.println("Вся сума: " + money);
-        System.out.println("Відсоток податку: " + taxRate);
-        System.out.println("Сума податку: " + (money * taxRate / 100));
+        System.out.printf("Вся сума: %d%nВідсоток податку: %.2f%nСума податку: %.2f%n", money, taxRate, (money * taxRate / 100));
     }
 }
